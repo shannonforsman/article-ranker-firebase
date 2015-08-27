35c166378a7f769e7fc60538ff2f999d00feb6c5
Shannon Forsman <sforsman1@gmail.com>
2015-08-27 11:51:44 -0600

    refactored to use firebase

--- javascript/app.js
+++ javascript/app.js
@@ -1,62 +1,62 @@
-var app = angular.module("redditApp", ['angularMoment', 'ngAnimate']);
+var app = angular.module("redditApp", ['angularMoment', 'ngAnimate', 'firebase']);
   app.run(function(amMoment) {
     amMoment.changeLocale('de');
 });
 
-var link = 'https://desolate-ocean-9111.herokuapp.com/'
+app.controller("RedditInfoController", function($scope, $http, $firebaseArray){
 
-app.controller("RedditInfoController", function($scope, $http){
-  $scope.list = []
-  $http.get(link + 'articles').
-  then(function(response) {
-    $scope.list = response.data
-  }, function(response) {
-    console.log(response)
-  });
+  var data = new Firebase('https://article-ranker.firebaseio.com/posts')
+  $scope.posts = $firebaseArray(data)
   $scope.votes = 0
-  $scope.upVote = function(id) {
-    $scope.list[id].votes = parseInt($scope.list[id].votes, 10) + 1
-    $http.put(link + 'articles', {'id': id, 'upvote': true})
+
+  $scope.upVote = function(post) {
+    post.votes ++
+    $scope.posts.$save(post)
   }
-  $scope.downVote = function(id) {
-    $scope.list[id].votes = parseInt($scope.list[id].votes, 10) - 1
-    $http.put(link + 'articles', {'id': id, 'upvote': false})
+  $scope.downVote = function(post) {
+    post.votes --
+    $scope.posts.$save(post)
   }
   $scope.submit = function(title, author, imgUrl, description) {
     var obj = {}
     $scope.showDetails = ! $scope.showDetails
+    var date = new Date()
     obj['title'] = title
     obj['author'] = author
     obj['imgUrl'] = imgUrl
     obj['description'] = description
-    obj['date'] = new Date()
+    obj['date'] = date.toString()
     obj['votes'] = 0
-    obj['id'] = $scope.list.length
-    obj['comments'] = []
-
-    $http.post(link + 'articles', obj).
-    then(function(response) {
-      $scope.list = response.data
-    }, function(response) {
-      console.log(response)
-    });
+    //
     $scope.title = ''
     $scope.author = ''
-    $scope.imgUrl = ''
+    $scope.imgUrl = null
     $scope.description = ''
+
+    $scope.posts.$add(obj)
   }
-  $scope.commentSubmit = function(author, text, id, post) {
+
+  $scope.commentSubmit = function(author, text, post) {
     var obj = {}
     post.addComment = ! post.addComment
+
     obj['author'] = author
     obj['text'] = text
-    $scope.list[id].comments.push(obj)
-    $http.put(link + 'comments', {'id': id, 'comments': obj}).
-    then(function(response) {
-      console.log(response)
-      $scope.list[id] = response.data
-    })
+    console.log(post)
+    if (post.comments === undefined) {
+      post.comments = []
+    }
+    post.comments.push(obj)
+    $scope.posts.$save(post)
+    post.author = ''
+    post.text = ''
   }
-  $scope.author = ''
-  $scope.text = ''
+  $scope.showComments = function(post) {
+    post.showComment = ! post.showComment
+    $scope.posts.$save(post)
+  }
+  $scope.addComments = function(post) {
+    post.addComment = ! post.addComment
+    $scope.posts.$save(post)
+  }
 })
